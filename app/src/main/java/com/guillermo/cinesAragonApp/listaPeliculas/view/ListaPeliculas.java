9e1bbbd9254f9460e3dc2ef596dd796dc1fa5f29
SquidoCode <squidocode@hotmail.com>
2021-06-01 20:36:22 +0200

    menu completado, faltan las funcionalidades

--- app/src/main/java/com/guillermo/cinesAragonApp/listaPeliculas/view/ListaPeliculas.java
+++ app/src/main/java/com/guillermo/cinesAragonApp/listaPeliculas/view/ListaPeliculas.java
@@ -1,14 +1,17 @@
 package com.guillermo.cinesAragonApp.listaPeliculas.view;
 
 import android.os.Bundle;
-import android.text.Editable;
-import android.text.TextWatcher;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.AutoCompleteTextView;
+import android.widget.Spinner;
 import android.widget.Toast;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -28,6 +31,9 @@ public class ListaPeliculas extends AppCompatActivity implements ContratoListaPe
     private RecyclerView recyclerView;
     private RecyclerView.LayoutManager layoutManager;
     private NavigationView navigationView;
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle actionBarDrawerToggle;
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,7 +43,28 @@ public class ListaPeliculas extends AppCompatActivity implements ContratoListaPe
         navigationView = (NavigationView) findViewById(R.id.nav_view);
         presentadorListaPeliculas = new PresentadorListaPeliculas(this, this);
         presentadorListaPeliculas.getPeliculas(false);
-//        filtrado();
+        filtrado();
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.drawer_opened, R.string.drawer_closed) {
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                actionBarDrawerToggle.syncState();
+            }
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+                actionBarDrawerToggle.syncState();
+
+            }
+        };
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
     }
 
 
@@ -60,39 +87,71 @@ public class ListaPeliculas extends AppCompatActivity implements ContratoListaPe
     }
 
 
+    //    public void filtrado() {
+//        Log.d(TAG, "[filtrado]");
+//
+//        final AutoCompleteTextView spinner = (AutoCompleteTextView) navigationView.getMenu().findItem(R.id.menu2);
+//
+//        String[] generos = new String[]{"todos", "acción", "aventura", "Terror", "Ciencia ficcion"};
+//        ArrayAdapter<String> adapterFiltro = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, generos);
+//        adapterFiltro.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+//        spinner.setAdapter(adapterFiltro);
+//
+//
+//        spinner.addTextChangedListener(new TextWatcher() {
+//            @Override
+//            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+//
+//            }
+//
+//            @Override
+//            public void onTextChanged(CharSequence s, int start, int before, int count) {
+//                if ("todos".contentEquals(s)) {
+//                    presentadorListaPeliculas.getPeliculas(false);
+//                } else {
+//                    presentadorListaPeliculas.setFiltro(String.valueOf(s));
+//                    presentadorListaPeliculas.getPeliculas(true);
+//                }
+//            }
+//
+//            @Override
+//            public void afterTextChanged(Editable s) {
+//
+//            }
+//        });
+//
+//    }
     public void filtrado() {
-        Log.d(TAG, "[filtrado]");
 
-        final AutoCompleteTextView spinner = (AutoCompleteTextView) navigationView.getMenu().findItem(R.id.menu1);
-
-        String[] generos = new String[]{"todos", "acción", "aventura", "Terror", "Ciencia ficcion"};
+        Spinner spinner = (Spinner) navigationView.getMenu().findItem(R.id.menu2).getActionView();
+        String[] generos = new String[]{"todos", "acción", "aventura", "terror", "ciencia ficcion"};
         ArrayAdapter<String> adapterFiltro = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, generos);
         adapterFiltro.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapterFiltro);
 
-
-        spinner.addTextChangedListener(new TextWatcher() {
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-                if ("todos".contentEquals(s)) {
-                    presentadorListaPeliculas.getPeliculas(false);
-                } else {
-                    presentadorListaPeliculas.setFiltro(String.valueOf(s));
-                    presentadorListaPeliculas.getPeliculas(true);
+            public void onItemSelected(AdapterView adapter, View v, int i, long lng) {
+                String selecteditem = adapter.getItemAtPosition(i).toString();
+                String seleccion = selecteditem;
+                switch (seleccion) {
+                    case "Filtro por genero:":
+                        return;
+                    case "todos":
+                        presentadorListaPeliculas.getPeliculas(false);
+                        break;
+                    default:
+                        presentadorListaPeliculas.setFiltro(seleccion);
+                        presentadorListaPeliculas.getPeliculas(true);
+                        break;
                 }
             }
 
             @Override
-            public void afterTextChanged(Editable s) {
-
+            public void onNothingSelected(AdapterView<?> parentView) {
+                return;
             }
         });
-
     }
 
 }
